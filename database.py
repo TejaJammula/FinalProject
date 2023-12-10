bbb7d6ef626044c4c0acb69a850369f2137e3c7a
TejaJammula <146783293+TejaJammula@users.noreply.github.com>
2023-12-10 02:14:55 -0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conn = sqlite3.connect('mobile_company.db')
+cursor = conn.cursor()
+
+# Create tables
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS mobiles (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ title TEXT NOT NULL,
+ release_year INTEGER,
+ company_id INTEGER,
+ FOREIGN KEY (company_id) REFERENCES companies(id)
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS companies (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ name TEXT NOT NULL
+)
+''')
+
+# Sample data for testing
+cursor.execute("INSERT INTO companies (name) VALUES ('Samsung')")
+cursor.execute("INSERT INTO companies (name) VALUES ('Apple')")
+cursor.execute("INSERT INTO mobiles (title, release_year, company_id) VALUES ('Mobile 1', 2020, 1)")
+cursor.execute("INSERT INTO mobiles (title, release_year, company_id) VALUES ('Mobile 2', 2021, 2)")
+
+conn.commit()
